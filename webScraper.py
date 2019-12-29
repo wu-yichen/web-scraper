df56411808750781ace796217ab31e12c7c7e09f
yichen <wuyichen1987@gmail.com>
2019-12-29 23:06:48 +0000

    initial commit

---
+++ webScraper.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+import pprint
+
+resp = requests.get('https://news.ycombinator.com/news')
+
+soup = BeautifulSoup(resp.text, 'html.parser')
+
+content = soup.select('.storylink')
+votes = soup.select('.subtext')
+
+result = []
+for index, item in enumerate(content):
+    text = content[index].getText()
+    href = content[index].get('href', None)
+    points = votes[index].select('.score')
+    if len(points):
+        point = int(points[0].getText().replace(' points', ''))
+        if point > 50:
+            result.append({'text': text, 'href': href, 'point': point})
+
+pprint.pprint(sorted(result, key=lambda k: k['point'], reverse=True))
